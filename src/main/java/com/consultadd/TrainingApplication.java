77056bbea688a42d14542853f2bcd8e0e02562d5
Prerna Chitransh <chitranshcse@gmail.com>
2021-11-14 23:01:08 +0530

    API

--- src/main/java/com/consultadd/TrainingApplication.java
+++ src/main/java/com/consultadd/TrainingApplication.java
@@ -2,7 +2,12 @@ package com.consultadd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableAuthorizationServer
+@EnableResourceServer
 @SpringBootApplication
 public class TrainingApplication {
 
